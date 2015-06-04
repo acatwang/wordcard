10db5fce2a3a6dba19725f6e995a5ba1b018aa2b
Yi-Yin Wang <acatwang@gmail.com>
2015-06-04 14:22:14 -0400

    search function in popup page

--- statics/js/search.js
+++ statics/js/search.js
@@ -1,7 +1,8 @@
 
 function searchDict(word){
+	// need to relax content_security_policy in order to run the script
+	var dictAPI = "https://api.wordnik.com/v4/word.json/"+word+"/definitions?limit=200&includeRelated=true&sourceDictionaries=webster&useCanonical=true&includeTags=false&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5";
 	
-	var dictAPI = "http://api.wordnik.com:80/v4/word.json/"+word+"/definitions?limit=200&includeRelated=true&sourceDictionaries=webster&useCanonical=true&includeTags=false&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5";
 	// Use JSONP to send cross-domain request
 	$.ajax({
 		url: dictAPI,
@@ -12,17 +13,28 @@ function searchDict(word){
 	    // Tell jQuery we're expecting JSONP
 	    dataType: "jsonp",
 	    success: function( data ) {
-	    	console.log('success');
+	    	console.debug('success');
         	console.log( data ); // server response
+        	showDefinition(data);
     	},
     	error: function (xhr, status, error) {
+    		alert(status);
         	console.log(xhr.responseText);
-        	console.log(status);
+        	console.debug(status);
     	},
 	})
 }
 
+function showDefinition(data){
+	$('#definition').show();
+	// only show the first definition for testing
+	$('#definition').text(data[0].text);
+
+}
+
 $('#searchbtn').click(function(){
+	$('#definition').empty();
+	var lookupword = $('#lookupword').val()? $('#lookupword').val():'test' ;
 	console.log('searching');
-	 	searchDict($('#lookupword').val());
+	searchDict(lookupword);
 });
