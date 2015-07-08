0a4f72dc7efa2a0617affbfad441e55d93f7e66b
Yi-Yin Wang <acatwang@gmail.com>
2015-07-08 00:28:14 -0700

    angular carousel and add new word

--- statics/js/cardApp.js
+++ statics/js/cardApp.js
@@ -1,14 +1,14 @@
 //angular
 (function(){
-	var app = angular.module('showcard', []);
+	var app = angular.module('showcard', ['ui.bootstrap']);
 
 	// Retrieve cards
-	var getAPI = "https://wordcard.herokuapp.com/api/v1/words/from_user.json?user_id=1&limit=10&offset=0";
+	var API_URL = "https://wordcard.herokuapp.com/api/v1/words/from_user.json?user_id=1&limit=10&offset=0";
 
 	app.factory('getService', function($http){
 		return {
 			getFoo: function(){
-				return $http.get(getAPI).then(function(result){
+				return $http.get(API_URL).then(function(result){
 					return result.data;
 				})
 			}
@@ -16,9 +16,22 @@
 	})
   
   	app.controller('cardController', function($scope, getService){
-  		getService.getFoo().then(function(data){
-  			$scope.cards = data;
+
+  	})
+
+	app.controller('carouselCtrl',function($scope,getService){
+		console.log($scope);
+		console.log(this);
+		getService.getFoo().then(function(data){
+  			$scope.slides = data;
   			console.log(data);
   		})
-  	})
+
+		$scope.addCard = function(){
+			console.log($scope);
+			$scope.slides.unshift({content:$scope.newWord});
+			$scope.newWord="";
+
+		}
+	})
 })();
--- statics/js/main.js
+++ statics/js/main.js
@@ -1,12 +1,12 @@
 
 $(document).ready(function(){
-
-	//carousel
-	 $('.slide-cards').slick({
-	 	slidesToShow: 1,
-	 	centerMode: true,
-		variableWidth: true
-
-	 });
+    //
+	////carousel
+	// $('.slide-cards').slick({
+	// 	slidesToShow: 1,
+	// 	centerMode: true,
+	//	variableWidth: true
+    //
+	// });
 
 });
